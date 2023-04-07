52ce3b736f0aa63de38718b56e54e73cef60f15d
robde <robde@LAPTOP-KM82B2SA>
2023-04-07 16:17:43 -0400

    Add project files.

---
+++ Program2/HotelForm.Designer.cs
@@ -0,0 +1,162 @@
+﻿namespace Program2
+{
+    partial class HotelForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.guestsLbl = new System.Windows.Forms.Label();
+            this.nightsLbl = new System.Windows.Forms.Label();
+            this.starsLbl = new System.Windows.Forms.Label();
+            this.costLbl = new System.Windows.Forms.Label();
+            this.guestsTxt = new System.Windows.Forms.TextBox();
+            this.nightsTxt = new System.Windows.Forms.TextBox();
+            this.starsCombo = new System.Windows.Forms.ComboBox();
+            this.calcBtn = new System.Windows.Forms.Button();
+            this.costOutputLbl = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // guestsLbl
+            // 
+            this.guestsLbl.AutoSize = true;
+            this.guestsLbl.Location = new System.Drawing.Point(146, 83);
+            this.guestsLbl.Name = "guestsLbl";
+            this.guestsLbl.Size = new System.Drawing.Size(117, 16);
+            this.guestsLbl.TabIndex = 0;
+            this.guestsLbl.Text = "Number of Guests:";
+            // 
+            // nightsLbl
+            // 
+            this.nightsLbl.AutoSize = true;
+            this.nightsLbl.Location = new System.Drawing.Point(149, 133);
+            this.nightsLbl.Name = "nightsLbl";
+            this.nightsLbl.Size = new System.Drawing.Size(113, 16);
+            this.nightsLbl.TabIndex = 1;
+            this.nightsLbl.Text = "Number of Nights:";
+            // 
+            // starsLbl
+            // 
+            this.starsLbl.AutoSize = true;
+            this.starsLbl.Location = new System.Drawing.Point(149, 192);
+            this.starsLbl.Name = "starsLbl";
+            this.starsLbl.Size = new System.Drawing.Size(76, 16);
+            this.starsLbl.TabIndex = 2;
+            this.starsLbl.Text = "Hotel Stars:";
+            // 
+            // costLbl
+            // 
+            this.costLbl.AutoSize = true;
+            this.costLbl.Location = new System.Drawing.Point(146, 331);
+            this.costLbl.Name = "costLbl";
+            this.costLbl.Size = new System.Drawing.Size(69, 16);
+            this.costLbl.TabIndex = 3;
+            this.costLbl.Text = "Hotel Cost";
+            // 
+            // guestsTxt
+            // 
+            this.guestsTxt.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.guestsTxt.Location = new System.Drawing.Point(287, 83);
+            this.guestsTxt.Name = "guestsTxt";
+            this.guestsTxt.Size = new System.Drawing.Size(100, 22);
+            this.guestsTxt.TabIndex = 4;
+            // 
+            // nightsTxt
+            // 
+            this.nightsTxt.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.nightsTxt.Location = new System.Drawing.Point(287, 145);
+            this.nightsTxt.Name = "nightsTxt";
+            this.nightsTxt.Size = new System.Drawing.Size(100, 22);
+            this.nightsTxt.TabIndex = 5;
+            // 
+            // starsCombo
+            // 
+            this.starsCombo.FormattingEnabled = true;
+            this.starsCombo.Items.AddRange(new object[] {
+            "1",
+            "2",
+            "3",
+            "4",
+            "5"});
+            this.starsCombo.Location = new System.Drawing.Point(287, 202);
+            this.starsCombo.Name = "starsCombo";
+            this.starsCombo.Size = new System.Drawing.Size(121, 24);
+            this.starsCombo.TabIndex = 6;
+            // 
+            // calcBtn
+            // 
+            this.calcBtn.Location = new System.Drawing.Point(287, 260);
+            this.calcBtn.Name = "calcBtn";
+            this.calcBtn.Size = new System.Drawing.Size(84, 32);
+            this.calcBtn.TabIndex = 7;
+            this.calcBtn.Text = "Calculate";
+            this.calcBtn.UseVisualStyleBackColor = true;
+            this.calcBtn.Click += new System.EventHandler(this.CalcBtn_Click);
+            // 
+            // costOutputLbl
+            // 
+            this.costOutputLbl.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.costOutputLbl.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.costOutputLbl.Location = new System.Drawing.Point(287, 331);
+            this.costOutputLbl.Name = "costOutputLbl";
+            this.costOutputLbl.Size = new System.Drawing.Size(100, 24);
+            this.costOutputLbl.TabIndex = 8;
+            // 
+            // HotelForm
+            // 
+            this.AcceptButton = this.calcBtn;
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.costOutputLbl);
+            this.Controls.Add(this.calcBtn);
+            this.Controls.Add(this.starsCombo);
+            this.Controls.Add(this.nightsTxt);
+            this.Controls.Add(this.guestsTxt);
+            this.Controls.Add(this.costLbl);
+            this.Controls.Add(this.starsLbl);
+            this.Controls.Add(this.nightsLbl);
+            this.Controls.Add(this.guestsLbl);
+            this.Name = "HotelForm";
+            this.Text = "Program 2 - Hotel Calculator";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label guestsLbl;
+        private System.Windows.Forms.Label nightsLbl;
+        private System.Windows.Forms.Label starsLbl;
+        private System.Windows.Forms.Label costLbl;
+        private System.Windows.Forms.TextBox guestsTxt;
+        private System.Windows.Forms.TextBox nightsTxt;
+        private System.Windows.Forms.ComboBox starsCombo;
+        private System.Windows.Forms.Button calcBtn;
+        private System.Windows.Forms.Label costOutputLbl;
+    }
+}
+
---
+++ Program2/HotelForm.cs
@@ -0,0 +1,110 @@
+﻿// Program 2
+// CIS 199-03
+// Due by: 3/11/22
+// Written by: S2210
+
+// This program calculates the cost of a hotel stay based on user inputed
+// data for the number of guests, number of nights, and hotel star rating.
+
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Program2
+{
+    public partial class HotelForm : Form
+    {
+        public HotelForm()
+        {
+            InitializeComponent();
+        }
+
+        private void CalcBtn_Click(object sender, EventArgs e)
+        {
+            int numGuests;                      // number of guests on reservation
+            int numNights;                      // number of nights of reservation
+            const int MIN_GUESTS = 1;           // minimum number of guests allowed per room
+            const int MAX_GUESTS = 7;           // maximum number of guests allowed per room
+            const int MIN_NIGHTS = 1;           // minimum number of night reservation required
+            const int NIGHTS_THRESHOLD1 = 7;    // threshold to reduce per night cost
+            const int NIGHTS_THRESHOLD2 = 31;   // threshold to reduce per night cost
+            const double GUEST_FEE1 = 100;      // guest fee for 1 guest per room
+            const double GUEST_FEE2 = 150;      // guest fee for 2 guests per room
+            const double GUEST_FEE3 = 250;      // guest fee for 3 guests per room
+            const double GUEST_FEE4 = 400;      // guest fee for 4-7 guests per room
+            const double NIGHT_FEE1 = 100;      // night fee for stays of 1-6 nights
+            const double NIGHT_FEE2 = 75;       // night fee for stays of 7-30 nights
+            const double NIGHT_FEE3 = 25;       // night fee for stays of 31+ nights
+            const double STAR_FACTOR1 = 1.0;    // multiple for hotel star rating of 1
+            const double STAR_FACTOR2 = 1.5;    // multiple for hotel star rating of 2
+            const double STAR_FACTOR3 = 2.5;    // multiple for hotel star rating of 3
+            const double STAR_FACTOR4 = 3.0;    // multiple for hotel star rating of 4
+            const double STAR_FACTOR5 = 4.0;    // multiple for hotel star rating of 5
+            double guestFee;                    // fee related to number of guests
+            double nightFee;                    // fee related to number of night stay
+            double starFactor;                  // fee multiple related to hotel star rating
+            double totalCost;                   // total cost of stay
+
+            // validate user input for number of guests
+            if (int.TryParse(guestsTxt.Text, out numGuests) && numGuests >= MIN_GUESTS && numGuests <= MAX_GUESTS)
+            {
+                // validate user input for number of nights
+                if (int.TryParse(nightsTxt.Text, out numNights) && numNights >= MIN_NIGHTS)
+                {
+                    // validate user input for number of hotel star rating
+                    if (starsCombo.SelectedIndex >= 0)
+                    {
+                        // determine the value of the star factor
+                        if (int.Parse(starsCombo.Text) == 1)
+                            starFactor = STAR_FACTOR1;
+                        else if (int.Parse(starsCombo.Text) == 2)
+                            starFactor = STAR_FACTOR2;
+                        else if (int.Parse(starsCombo.Text) == 3)
+                            starFactor = STAR_FACTOR3;
+                        else if (int.Parse(starsCombo.Text) == 4)
+                            starFactor = STAR_FACTOR4;
+                        else
+                            starFactor = STAR_FACTOR5;
+
+                        // determine the guest fee
+                        if (numGuests == MIN_GUESTS)
+                            guestFee = GUEST_FEE1;
+                        else if (numGuests == 2)
+                            guestFee = GUEST_FEE2;
+                        else if (numGuests == 3)
+                            guestFee = GUEST_FEE3;
+                        else
+                            guestFee = GUEST_FEE4;
+
+                        // determine the night fee
+                        if (numNights < NIGHTS_THRESHOLD1)
+                            nightFee = NIGHT_FEE1;
+                        else if (numNights < NIGHTS_THRESHOLD2)
+                            nightFee = NIGHT_FEE2;
+                        else
+                            nightFee = NIGHT_FEE3;
+
+                        // calculate the total cost and display the results
+                        totalCost = (guestFee + (numNights * nightFee)) * starFactor;
+                        costOutputLbl.Text = $"{totalCost:C}";
+                    }
+                    // reponse for invalid star rating selection
+                    else
+                        MessageBox.Show($"Invalid input. Select the number of hotel stars.");
+                }
+                // reponse for invalid number of nights input
+                else
+                    MessageBox.Show($"Invalid input. Enter the number of nights: {MIN_NIGHTS}+.");
+            }
+            // reponse for invalid number of guests input
+            else
+                MessageBox.Show($"Invalid input. Enter the number of guests from {MIN_GUESTS}-{MAX_GUESTS}.");
+        }
+    }
+}
